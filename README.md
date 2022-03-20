cc0c71c9af16ba7adefac36504b41c53d3c4ea59
Daniel Kucera <daniel.kucera@gmail.com>
2022-03-20 09:07:39 +0100

    v2.0

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# ESP8266 to ebus adapter with power from ebus
+
+Power consumption:
+
+- 22mA@20V
+- 36mA@10V
---
+++ panelize.sh
@@ -0,0 +1,10 @@
+kikit panelize --layout 'grid; rows: 3; cols: 2; hspace: 2mm; vspace: 0.5mm' \
+	--source 'tolerance: 50mm' \
+	--tabs 'fixed; width: 5mm' \
+	--cuts 'mousebites' \
+	--framing 'railslr; width: 3mm; space: 1.5mm' \
+	--fiducials '3fid; hoffset: 1.5mm; voffset: 1.5mm; coppersize: 2mm; opening: 1mm;' \
+	wiring.kicad_pcb panel.kicad_pcb
+
+#	--cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.1mm' \
+kikit fab jlcpcb --no-drc --assembly --schematic wiring.kicad_sch panel.kicad_pcb jlcpcb
