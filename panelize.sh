96603aee6775e5e230cdd32c27ec6ca2f998f836
Daniel Kucera <daniel.kucera@gmail.com>
2023-06-16 08:48:14 +0200

    v6.0

--- panelize.sh
+++ panelize.sh
@@ -1,10 +1,10 @@
 kikit panelize --layout 'grid; rows: 3; cols: 2; hspace: 2mm; vspace: 1mm; alternation: cols; rotation: 180deg;' \
 	--source 'tolerance: 50mm' \
-	--tabs 'fixed; vcount: 0; width: 9mm;' \
+	--tabs 'fixed; vcount: 0; width: 5mm;' \
 	--cuts 'mousebites' \
 	--framing 'railslr; width: 3mm; space: 1.5mm' \
 	--fiducials '3fid; hoffset: 1.5mm; voffset: 1.5mm; coppersize: 2mm; opening: 1mm;' \
 	wiring.kicad_pcb panel.kicad_pcb
 
 #	--cuts 'mousebites; drill: 0.5mm; spacing: 1mm; offset: -0.1mm' \
-kikit fab jlcpcb --no-drc --assembly --schematic wiring.kicad_sch panel.kicad_pcb fab-`date --iso-8601=seconds`
+kikit fab jlcpcb --no-drc --assembly --field LCSC,LCSC2 --schematic wiring.kicad_sch panel.kicad_pcb fab-`date --iso-8601=seconds`
